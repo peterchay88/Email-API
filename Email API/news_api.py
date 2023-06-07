0f576a53daa4d737cef50a44e1a81e4066f387cf
Peter <peter@Peters-MBP.peter.lan>
2023-06-07 12:52:37 -0600

    Added links, specified english news only, added subject line to the emails.

--- Email API/news_api.py
+++ Email API/news_api.py
@@ -3,7 +3,7 @@ import ssl
 import os
 import requests
 
-url = "https://newsapi.org/v2/top-headlines?sources=techcrunch&apiKey=df45f55e19b54b649ff36bf2178e407f"
+url = "https://newsapi.org/v2/top-headlines?sources=techcrunch&apiKey=df45f55e19b54b649ff36bf2178e407f&language=en"
 api_key = os.getenv("news_api")
 
 r = requests.get(url)
@@ -12,14 +12,17 @@ content = r.json()
 author = []
 title = []
 description = []
+url = []
 for articles in content["articles"]:
     author.append(articles["author"])
     title.append(articles["title"])
     description.append(articles["description"])
+    url.append(articles["url"])
 
 message = ""
-for authors, titles, descriptions in zip(author, title, description):
-    message += f"{authors}\n{titles}\n{descriptions}\n\n"
+for authors, titles, descriptions, urls in zip(author, title, description, url):
+    message += f"{authors}\n{titles}\n{descriptions}\n{urls}\n\n"
+message = f"Subject: Today's News\n{message}"
 message = message.encode("utf-8")
 
 host = "smtp.gmail.com"
