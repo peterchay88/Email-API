6653d0fc9ba783bc91870d68cee1d0cb2c54586f
Peter <peter@Peters-MBP.peter.lan>
2023-06-07 12:21:43 -0600

    finished the student exercise.

--- Email API/news_api.py
+++ Email API/news_api.py
@@ -3,6 +3,25 @@ import ssl
 import os
 import requests
 
+url = "https://newsapi.org/v2/top-headlines?sources=techcrunch&apiKey=df45f55e19b54b649ff36bf2178e407f"
+api_key = "df45f55e19b54b649ff36bf2178e407f"
+
+r = requests.get(url)
+content = r.json()
+
+author = []
+title = []
+description = []
+for articles in content["articles"]:
+    author.append(articles["author"])
+    title.append(articles["title"])
+    description.append(articles["description"])
+
+message = ""
+for authors, titles, descriptions in zip(author, title, description):
+    message += f"{authors}\n{titles}\n{descriptions}\n\n"
+message = message.encode("utf-8")
+
 host = "smtp.gmail.com"
 port = 465
 
@@ -11,8 +30,6 @@ password = os.getenv("PASSWORD")
 
 receiver = "peterchay112@gmail.com"
 context = ssl.create_default_context()
-
-message = "This is a test"
 
 with smtplib.SMTP_SSL(host, port, context=context) as server:
     server.login(username, password)
